75709bc950875233b1d996492cfb0de519c7e968
Butler Steve <xry3440@hca.corpad.net>
2023-04-12 20:09:29 -0500

    update styling

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
+import JokeSetup from './jokeSetup';
+
 function Home() {
   return (
     <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
+      className="text-center w-80 d-flex flex-column "
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        // maxWidth: '400px',
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h1 className="text-light display-1 w-75 mx-auto">The Joke Generator</h1>
+      <JokeSetup />
     </div>
   );
 }
